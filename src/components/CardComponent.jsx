af168f02d58bbb12244ee3bfff268d0f97a69f7f
Yoojeong Lee <yjclarelee@postech.ac.kr>
2021-10-04 19:23:20 +0900

    Show data from json-server

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -3,7 +3,7 @@ import { Card, Modal } from 'antd';
 
 const { Meta } = Card;
 
-function CardComponent() {
+function CardComponent(props) {
   const [isModalVisible, setIsModalVisible] = useState(false);
 
   return (
@@ -14,16 +14,12 @@ function CardComponent() {
         cover={<img alt="example" src="https://images.unsplash.com/photo-1523800503107-5bc3ba2a6f81?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1180&q=80"/>}
         onClick={() => setIsModalVisible(true)}
       >
-        <Meta title="Europe Street beat" description="www.instagram.com" />
+        <Meta title={props.title} description={props.body} />
       </Card>
-      <Modal title="Basic Modal" visible={isModalVisible} onCancel={() => setIsModalVisible(false)} onOk={() => setIsModalVisible(false)}>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
+      <Modal title={props.title} visible={isModalVisible} onCancel={() => setIsModalVisible(false)} onOk={() => setIsModalVisible(false)}>
+      <p>{props.body}</p>
     </Modal>
-    </div>
-    
-      
+    </div>  
   );
 }
 
