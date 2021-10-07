a80b5447dc60dd898a2efcbaf666974689cc0194
chaekie <chaekie.kim@gmail.com>
2021-10-07 19:03:20 +0900

    태그 필터링 기능 구현

---
+++ src/components/TagFiltering.jsx
@@ -0,0 +1,26 @@
+import { Button } from "antd";
+import React from "react";
+
+const TagFiltering = ({ onFiltering }) => {
+  const categoryList = ["ALL", "Java", "Python", "JavaScript"];
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        columnGap: "5px",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {categoryList.map((category, idx) => (
+        <Button onClick={onFiltering} key={idx} shape="round">
+          {category}
+        </Button>
+      ))}
+    </div>
+  );
+};
+
+export default TagFiltering;
