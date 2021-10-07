07b8a5d5199fdd6fab0fdb48ac578178e0396fbb
500er <404500er@gmail.com>
2021-10-07 10:54:51 +0900

    feat: update, delete edit

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -10,24 +10,26 @@ const { Meta } = Card;
 function CardComponent(props) {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const { visible, loading } = useState(false);
-  const [postNum, setPostNum] = useState(1);
+  const [postNum, setPostNum] = useState([]);
   const [editing, setEditing] = useState(true);
 
   const onDel = async (newPost) => {
-    await axios.delete("http://localhost:4000/posts?_sort=id", {
+    await axios.delete(`http://localhost:4000/posts/${props.id}`, {
       ...newPost,
     });
-    newPost.filter(postNum => this.id != postNum.id);
-    // setPostNum(postNum - 1);
+    console.log('delete')
     setEditing(true);
   };
-  // const onDel = postNum.filter(postNum => this.id != postNum.id);
 
   const onUpdate = async (newPost) => {
-    await axios.patch("http://localhost:4000/posts?_sort=id", {
-      ...newPost,
-    });
-    setPostNum(postNum + 0);
+    await axios.patch(`http://localhost:4000/posts/${props.id}`, JSON.stringify({
+      data: {
+        id: '`${id}`',
+        title: '`${title}`',
+        body: '`${body}`'
+      }
+    }));
+    console.log('patch')
     setEditing(false);
   };
 
@@ -39,8 +41,9 @@ function CardComponent(props) {
       okText: 'Yes',
       okType: 'danger',
       cancelText: 'No',
+      centered: 'yes',
       onOk() {
-        console.log('Yes');
+        // console.log('Yes');
         onDel(true);
         setEditing(true);
         setIsModalVisible(false)
@@ -60,24 +63,24 @@ function CardComponent(props) {
         cover={<img alt="example" src="https://images.unsplash.com/photo-1523800503107-5bc3ba2a6f81?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1180&q=80"/>}
         onClick={() => setIsModalVisible(true)}
       >
-        <Meta title={props.title} description={props.body} />
+        <div className="cardText">
+          <Meta title={props.title} description={props.body} />
+        </div>
       </Card>
-        <Modal visible={isModalVisible} onCancel={() => setIsModalVisible(false)} onOk={editing} footer={[]}>
+        <Modal visible={isModalVisible} onCancel={() => setIsModalVisible(false)} onOk={editing} footer={[]} centered>
         {editing ? (
           <>
-            <p>{props.title}</p>
-            <p>{props.body}</p>
-            <Button size="medium" onClick={() => setEditing(false)} >
-              수정
-            </Button>
-            <Button size="medium" onClick={showDeleteConfirm} type="danger" ghost>
-              삭제
-            </Button>
+            <h1 style={{marginTop: "30px"}}>{props.title}</h1>
+            <p style={{height: "200px", marginBottom: "20px"}}>{props.body}</p>
+            <div style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
+              <Button size="medium" onClick={() => setEditing(false)}>수정</Button>
+              <Button size="medium" onClick={showDeleteConfirm} type="danger" ghost>삭제</Button>
+            </div>
           </>
           ) : (
-            <div style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
-              <Input defaultValue={`${props.title}`} />
-              <TextArea defaultValue={`${props.body}`} />
+            <>
+              <Input defaultValue={`${props.title}`} style={{fontSize: "28px", padding: "0", marginTop: "30px"}}/>
+              <TextArea defaultValue={`${props.body}`} style={{height: "200px", marginTop: "10px", marginBottom: "20px", padding: "0"}}/>
               <div style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
               <Button size="medium" onClick={onUpdate} type="primary">
                 수정완료
@@ -86,7 +89,7 @@ function CardComponent(props) {
                 취소
               </Button>
               </div>
-            </div>
+            </>
           )}
       </Modal>
     </div>  
