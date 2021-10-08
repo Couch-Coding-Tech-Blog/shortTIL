2ce973ae20f02abcfd68012b47d2f3702aab65c2
chaekie <chaekie.kim@gmail.com>
2021-10-08 22:20:07 +0900

    무한스크롤 기능 구현(필터링 안되는 버그 존재)

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { Layout, Button, Input, Select } from "antd";
+import React, { useState, useEffect, useRef, useReducer } from "react";
+import { Layout, Button, Input, Select, Spin } from "antd";
 import { EditFilled } from "@ant-design/icons";
 import Navbar from "../components/Header/Header";
 import CardComponent from "../components/Card/CardComponent";
@@ -11,23 +11,100 @@ import TagFiltering from "../components/TagFiltering";
 const { Option } = Select;
 
 function Main({ imageUploader }) {
+  const [allData, setAllData] = useState([]);
   const [postData, setPostData] = useState([]);
   const [filtered, setFiltered] = useState(false);
   const [filteringData, setFilteringData] = useState([]);
   const [postNum, setPostNum] = useState(0);
   const [searchTerm, setSearchTerm] = useState("");
   const [searchType, setSearchType] = useState("title");
+  const perPage = 12;
+  const [element, setElement] = useState(null);
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "start":
+        return { ...state, loading: true };
+      case "loaded":
+        return {
+          ...state,
+          loading: false,
+          data: [...state.data, ...action.newData],
+          more: action.newData.length === perPage,
+          after: state.after + action.newData.length,
+        };
+      default:
+        throw Error("Don't understand action");
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    more: true,
+    data: [],
+    after: 0,
+  });
+  const load = () => {
+    dispatch({ type: "start" });
+    setTimeout(() => {
+      const newData = allData.slice(after, after + perPage);
+      console.log(allData);
+      dispatch({ type: "loaded", newData });
+    }, 300);
+  };
+  const { loading, data, after, more } = state;
+  const loader = useRef(load);
+
+  const observer = useRef(
+    new IntersectionObserver(
+      (entries) => {
+        const first = entries[0];
+        if (first.isIntersecting) {
+          loader.current();
+        }
+      },
+      { threshold: 1 }
+    )
+  );
+
+  useEffect(() => {
+    loader.current = load;
+  }, [load]);
 
   useEffect(() => {
     //searchTerm여부에 따라 전체검색 or 필터검색 실행
     searchTerm ? getFilterData() : getData();
   }, [searchTerm, searchType, postNum]);
 
+  useEffect(() => {
+    const currentElement = element;
+    const currentObserver = observer.current;
+    if (currentElement) {
+      currentObserver.observe(currentElement);
+    }
+    return () => {
+      if (currentElement) {
+        currentObserver.unobserve(currentElement);
+      }
+    };
+  }, [element]);
+
   async function getData() {
     const res = await axios.get(
       "http://localhost:4000/posts?_sort=id&_order=desc"
     );
-    setPostData(res.data);
+    setAllData(res.data);
+    setPostData(data);
+    const currentElement = element;
+    const currentObserver = observer.current;
+    if (currentElement) {
+      currentObserver.observe(currentElement);
+    }
+    return () => {
+      if (currentElement) {
+        currentObserver.unobserve(currentElement);
+      }
+    };
   }
 
   async function getFilterData() {
@@ -43,14 +120,12 @@ function Main({ imageUploader }) {
     });
     setPostNum(postNum + 1);
   };
-
   const handleSearchChange = async (event) => {
     setSearchTerm(event.target.value);
   };
   const handleSelectChange = (event) => {
     setSearchType(event);
   };
-
   const handleFiltering = (event) => {
     if (event.target.innerText === "ALL") {
       setFiltered(false);
@@ -89,7 +164,7 @@ function Main({ imageUploader }) {
   };
 
   return (
-    <div>
+    <>
       <Navbar
         onAdd={onAdd}
         handleSearchChange={handleSearchChange}
@@ -104,49 +179,63 @@ function Main({ imageUploader }) {
           <div className="cardheader">
             <p>All Posts</p>
             <Input.Group compact className="search">
-              <Select defaultValue="title" onChange={handleSelectChange} value={searchType}>
+              <Select
+                defaultValue="title"
+                onChange={handleSelectChange}
+                value={searchType}
+              >
                 <Option value="title">Title</Option>
                 <Option value="body">Body</Option>
                 <Option value="tags">Tags</Option>
               </Select>
-              <Input onChange={handleSearchChange} placeholder="search" className="input"/>
+              <Input
+                onChange={handleSearchChange}
+                placeholder="search"
+                className="input"
+              />
             </Input.Group>
           </div>
           <div className="grid">
             <div className="cards">
-            {filtered ? (
-              <>
-                {filteringData.map((post) => (
-                  <CardComponent
-                    key={post.id}
-                    id={post.id}
-                    title={post.title}
-                    body={post.body}
-                    imagefile={post.uploaded_images}
-                    imageUploader={imageUploader}
-                  ></CardComponent>
-                ))}
-              </>
-            ) : (
-              <>
-                {postData.map((post) => (
-                  <CardComponent
-                    key={post.id}
-                    id={post.id}
-                    title={post.title}
-                    body={post.body}
-                    imagefile={post.uploaded_images}
-                    imageUploader={imageUploader}
-                  ></CardComponent>
-                ))}
-              </>
-            )}
+              {filtered ? (
+                <>
+                  {filteringData.map((post) => (
+                    <CardComponent
+                      key={post.id}
+                      id={post.id}
+                      title={post.title}
+                      body={post.body}
+                      imagefile={post.uploaded_images}
+                      imageUploader={imageUploader}
+                    ></CardComponent>
+                  ))}
+                </>
+              ) : (
+                <>
+                  {data.map((post) => (
+                    <CardComponent
+                      key={post.id}
+                      id={post.id}
+                      title={post.title}
+                      body={post.body}
+                      imagefile={post.uploaded_images}
+                      imageUploader={imageUploader}
+                    ></CardComponent>
+                  ))}
+                </>
+              )}
             </div>
+            {loading && (
+              <div className="spin">
+                <Spin />
+              </div>
+            )}
+            {!loading && more && <div ref={setElement}></div>}
           </div>
         </div>
       </section>
       <footer>© {new Date().getFullYear()} TIL Team</footer>
-  </div>
+    </>
   );
 }
 
